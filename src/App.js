b84007cf2339fd60b579c0a7f697a3466898713b
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-06 21:22:45 +0300

    Import and add Calculator.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Calculator from './components/Calculator';
 
-const App = () => {
-  return <div className="App"></div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
 
 export default App;
