f8a2e5afd02d915eac7aefcde0f47f196117fffa
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-07 19:36:09 +0300

    Add props and onClick event listener

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,25 +1,43 @@
-const Buttons = () => (
+import PropTypes from 'prop-types';
+import Screen from './Screen';
+
+const Buttons = ({
+  handleClick, total, operation, next,
+}) => (
   <div className="buttons">
-    <button type="button" className="btn gray">AC</button>
-    <button type="button" className="btn gray">+/-</button>
-    <button type="button" className="btn gray">%</button>
-    <button type="button" className="btn orange">÷</button>
-    <button type="button" className="btn gray">7</button>
-    <button type="button" className="btn gray">8</button>
-    <button type="button" className="btn gray">9</button>
-    <button type="button" className="btn orange">x</button>
-    <button type="button" className="btn gray">4</button>
-    <button type="button" className="btn gray">5</button>
-    <button type="button" className="btn gray">6</button>
-    <button type="button" className="btn orange">-</button>
-    <button type="button" className="btn gray">1</button>
-    <button type="button" className="btn gray">2</button>
-    <button type="button" className="btn gray">3</button>
-    <button type="button" className="btn orange">+</button>
-    <button type="button" className="btn gray span-two">0</button>
-    <button type="button" className="btn gray">.</button>
-    <button type="button" className="btn orange">=</button>
+    <Screen total={Number(total)} operation={operation} next={next} />
+    <button type="button" className="btn gray" onClick={handleClick}>AC</button>
+    <button type="button" className="btn gray" onClick={handleClick}>+/-</button>
+    <button type="button" className="btn gray" onClick={handleClick}>%</button>
+    <button type="button" className="btn orange" onClick={handleClick}>÷</button>
+    <button type="button" className="btn gray" onClick={handleClick}>7</button>
+    <button type="button" className="btn gray" onClick={handleClick}>8</button>
+    <button type="button" className="btn gray" onClick={handleClick}>9</button>
+    <button type="button" className="btn orange" onClick={handleClick}>x</button>
+    <button type="button" className="btn gray" onClick={handleClick}>4</button>
+    <button type="button" className="btn gray" onClick={handleClick}>5</button>
+    <button type="button" className="btn gray" onClick={handleClick}>6</button>
+    <button type="button" className="btn orange" onClick={handleClick}>-</button>
+    <button type="button" className="btn gray" onClick={handleClick}>1</button>
+    <button type="button" className="btn gray" onClick={handleClick}>2</button>
+    <button type="button" className="btn gray" onClick={handleClick}>3</button>
+    <button type="button" className="btn orange" onClick={handleClick}>+</button>
+    <button type="button" className="btn gray span-two" onClick={handleClick}>0</button>
+    <button type="button" className="btn gray" onClick={handleClick}>.</button>
+    <button type="button" className="btn orange" onClick={handleClick}>=</button>
   </div>
 );
+
+Buttons.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  total: PropTypes.number.isRequired,
+  operation: PropTypes.string,
+  next: PropTypes.string,
+};
+
+Buttons.defaultProps = {
+  operation: null,
+  next: null,
+};
 
 export default Buttons;
