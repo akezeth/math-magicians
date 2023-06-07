ddb89a8e664e58fd377a0d41f557cab6838c46d3
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-07 19:35:28 +0300

    Add props

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,23 @@
-const Screen = () => (
+import PropTypes from 'prop-types';
+
+const Screen = ({ total, operation, next }) => (
   <div className="screen">
-    0
+    {total}
+    {operation}
+    {next}
   </div>
 );
+
+Screen.propTypes = {
+  total: PropTypes.number,
+  operation: PropTypes.string,
+  next: PropTypes.string,
+};
+
+Screen.defaultProps = {
+  total: '',
+  operation: null,
+  next: null,
+};
 
 export default Screen;
