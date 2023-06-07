c48a5ef1f9298f82a072f57ada322898b05beab3
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-07 19:28:22 +0300

    Add useState hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,19 @@
-import Screen from './Screen';
+import { useState } from 'react';
 import Buttons from './Buttons';
+import calculate from '../logic/calculator';
 
-const Calculator = () => (
-  <div className="wrapper">
-    <Screen />
-    <Buttons />
-  </div>
-);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    opeation: null,
+  });
+
+  return (
+    <div className="wrapper">
+      <Buttons />
+    </div>
+  );
+};
 
 export default Calculator;
